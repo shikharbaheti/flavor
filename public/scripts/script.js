9de1ce6d7a941d5945e9fea8311c7df2199ff374
Shikhar Baheti <shikharbaheti@gmail.com>
2020-04-28 18:50:13 -0500

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -72,6 +72,7 @@ function businesses() {
 
 // Converting HTML Template using the API data to cards
 function present(data) {
+    console.log(data);
     let x = data["businesses"].length;
     var source = document.getElementById("businessesDisplayTemplate").innerHTML;
     var template = Handlebars.compile(source);
